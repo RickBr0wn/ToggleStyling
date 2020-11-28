43a79a3521d9d4b7a0e4acc68e428877ae8b6c60
Rick Brown <ricky.brown.00@gmail.com>
2020-11-28 22:48:18 +0000

    Initial Commit

---
+++ ToggleStyling/ToggleStylingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToggleStylingApp.swift
+//  ToggleStyling
+//
+//  Created by Rick Brown on 28/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct ToggleStylingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
